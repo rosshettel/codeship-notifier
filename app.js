c536ef3468072026cc815e7352b9a07c2440449b
Ross Hettel <rhettel@highground.com>
2015-04-06 11:05:45 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+"use strict";
+
+var express = require('express'),
+	app = express(),
+	port = process.env.port || 3000;
+
+app.get('/', function (req, res) {
+	res.send('Hello World!');
+});
+
+app.post('/', function (req, res) {
+	console.log('received webhook', req.body);
+});
+
+app.listen(port);
