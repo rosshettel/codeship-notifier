60a3d34fa5e519e058ef37bea9a19073ffc24321
Ross Hettel <rhettel@highground.com>
2015-04-06 11:30:27 -0500

    added form body parser

--- app.js
+++ app.js
@@ -2,7 +2,10 @@
 
 var express = require('express'),
 	app = express(),
+	bodyParser = require('body-parser'),
 	port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
 	res.send('Hello World!');
@@ -10,6 +13,7 @@ app.get('/', function (req, res) {
 
 app.post('/', function (req, res) {
 	console.log('received webhook', req.body);
+	res.end();	//dont need to send anything back
 });
 
 app.listen(port);
