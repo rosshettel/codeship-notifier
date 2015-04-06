c37bea4bc4706e2d26534176344cfc6e88ab4011
Ross Hettel <rhettel@highground.com>
2015-04-06 11:57:57 -0500

    added hipchat integration

--- app.js
+++ app.js
@@ -3,6 +3,9 @@
 var express = require('express'),
 	app = express(),
 	bodyParser = require('body-parser'),
+	Hipchat = require('node-hipchat'),
+	HC = new Hipchat(process.env.hipchat_token),
+	devRoomId = '534835',
 	port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
@@ -12,7 +15,26 @@ app.get('/', function (req, res) {
 });
 
 app.post('/', function (req, res) {
-	console.log('received webhook', req.body);
+	var build;
+	
+	if (req.body && req.body.build) {
+		build = req.body.build;
+		console.log('received webhook', build);
+		
+		if (build.status === 'error') {
+			//notifiy hipchat here
+			HC.postMessage({
+				room: devRoomId,
+				from: 'Codeship',
+				message: '(failed) ' + build.committer + ' broke the build on (branch) ' + build.branch,
+				message_format: 'text',
+				color: 'red'
+			}, function (data, err) {
+				console.log('sent message', err);
+			});
+		}
+	}
+	
 	res.end();	//dont need to send anything back
 });
 
